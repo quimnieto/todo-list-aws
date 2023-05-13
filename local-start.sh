2ab291738916f1e5de6f11aa02b9094d083e3fe8
Ubuntu <ubuntu@ip-172-31-94-7.ec2.internal>
2023-05-13 10:21:21 +0000

    feature-1: improve local-start.sh

--- local-start.sh
+++ local-start.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
-## Crear red de docker
-docker network create sam
+## Create SAM docker network
+if [ ! "$(docker network ls | grep sam)" ]; then
+echo "Creating sam network ..."
+  docker network create sam
+else
+  echo "sam network exists."
+fi
 
-## Levantar el contenedor de dynamodb en la red de sam con el nombre de dynamodb
-docker run -p 8000:8000 --network sam --name dynamodb -d amazon/dynamodb-local
+## Create dynamodb container
+if [ ! "$(docker ps | grep dynamodb)" ]; then
+    if [ "$(docker ps -aq -f name=dynamodb)" ]; then
+        echo "Cleaning dynamodb ..."
+        docker rm dynamodb
+    fi
+    echo "Running dynamodb in sam network ..."
+    docker run -p 8000:8000 --network sam --name dynamodb -d amazon/dynamodb-local
+else
+  echo "Dynamodb already running."
+fi
 
-## Crear la tabla en local, para poder trabajar localmemte
+## Create dynamodb table
 aws dynamodb create-table --table-name local-TodosDynamoDbTable --attribute-definitions AttributeName=id,AttributeType=S --key-schema AttributeName=id,KeyType=HASH --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1 --endpoint-url http://localhost:8000 --region us-east-1
 
-## Empaquetar sam
-sam build --use-container # también se puede usar sam build --use-container si se dan problemas con las librerías de python
+## build sam project usinf container
+sam build --use-container
 
-## Levantar la api en local, en el puerto 8080, dentro de la red de docker sam
+## run api in local env
 sam local start-api --port 8081 --env-vars localEnvironment.json --docker-network sam
